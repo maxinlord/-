3f4d2fc9cd9d0de27f2c15478209d140ad9b8948
maxinlord <65973902+maxinlord@users.noreply.github.com>
2020-05-27 23:02:23 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,29 +1,41 @@
 import telebot
 import config
-import pickle
-import requests
-import random
+from telebot import types
 from telebot.types import Message
+from weather import Weather
 
 bot = telebot.TeleBot(config.TOKEN)
-USER = set()
+
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+itembtna = types.KeyboardButton('Author')
+itembtnb = types.KeyboardButton('Hello!')
+itembtnc = types.KeyboardButton('Мои данные')
+markup.row(itembtnc)
+markup.row(itembtna)
+markup.row(itembtnb)
 
 
 @bot.message_handler(commands=['start', 'help'])
-def command_handler(message):
+@bot.edited_message_handler(content_types=['text'])
+def welcome(message):
     bot.send_sticker(message.chat.id, config.STICKER_ID)
-    bot.send_message(message.chat.id,'Добро пожаловать!')
+    bot.send_message(message.chat.id, 'Добро пожаловать!')
+    bot.send_message(message.chat.id, "Выбери одну из кнопок:", reply_markup=markup)
+
 
 @bot.message_handler(content_types=['text'])
-@bot.edited_message_handler(content_types=['text'])
-def echo(message):
-    if 'Author' in message.text:
-        bot.send_message(message.chat.id,'@maxA0n')
-        return
-    bot.send_message(message.chat.id, 'Your name:' + ' ' + str(message.chat.first_name))
-    bot.send_message(message.chat.id, 'Your username:' + ' ' + '@' + str(message.chat.username))
-    bot.send_message(message.chat.id, 'Your id:' + ' ' + str(message.chat.id))
-    USER.add(message.from_user.id)
+def usser(message):
+    if 'Мои данные' in message.text:
+        bot.send_message(message.chat.id, 'Your name:' + ' ' + str(message.chat.first_name))
+        bot.send_message(message.chat.id, 'Your username:' + ' ' + '@' + str(message.chat.username))
+        bot.send_message(message.chat.id, 'Your id:' + ' ' + str(message.chat.id))
+    else:
+        if 'Hello!' in message.text:
+            bot.send_sticker(message.chat.id, config.STICKER_ID2)
+        else:
+            if 'Author' in message.text:
+                bot.send_message(message.chat.id, '@maxA0n')
+            return
 
 
 bot.polling(timeout=60)
